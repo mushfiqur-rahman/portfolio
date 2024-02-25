9f306faf5fe65d26f0379aabcdcf645519643604
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-02-25 17:47:06 +0600

    seo improved

--- components/About.jsx
+++ components/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </div>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-indigo-400 rounded-xl flex items-center p-4 hover:scale-105 ease-in duration-300">
-          <Image src={MushfiqImg} className="rounded-xl" alt="MushfiqImg" />
+          <Image src={MushfiqImg} className="rounded-xl" alt="MushfiqImg" priority/>
         </div>
       </div>
     </div>
