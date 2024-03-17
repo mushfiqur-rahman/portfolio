f2977fa12d25abf59e4bb1c77836dbd873301690
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-03-17 09:39:18 +0600

    right click protect

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body oncontextmenu="return false">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
