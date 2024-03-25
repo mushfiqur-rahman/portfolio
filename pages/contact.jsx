d64a6cbdc2dd3436972ac27d86bc95784a2b9fb2
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-03-25 21:53:31 +0600

    contact api removed

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -110,11 +110,7 @@ const Contact = () => {
 
             <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
               <div className="p-4">
-                <form
-                  action="https://getform.io/f/c5c082f5-315e-47c3-b3b7-e0c73974c08f"
-                  method=""
-                  encType="multipart/form-data"
-                >
+                <form action="" method="POST" encType="multipart/form-data">
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Your Name</label>
                     <input
