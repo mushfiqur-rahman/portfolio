d499151f902b7dd505bc492848015005300062a0
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-03-25 22:35:47 +0600

    post delete

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -110,7 +110,7 @@ const Contact = () => {
 
             <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
               <div className="p-4">
-                <form action="" method="POST" encType="multipart/form-data">
+                <form action="" method="" encType="multipart/form-data">
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Your Name</label>
                     <input
