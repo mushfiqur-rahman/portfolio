359c587135df87f8c489c223160aff2854f8a265
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-05-21 08:37:20 +0600

    schema update

--- components/SiteName.jsx
+++ components/SiteName.jsx
@@ -10,11 +10,6 @@ const SiteSchema = () => {
           name: "Mushfiqur Rahman Portfolio",
           description:
             "I'm Mushfiqur Rahman, this is my portfolio. I am looking for remote or hybrid job in python django webdeveloper position. I have been working at Upwork as a python django developer since 2022",
-          addrress: "Chuknagar, Dumuria, Khulna - 9252",
-          alumniOf: "ILACS(National University of Bangladesh), Khulha",
-          birthPlace: "Chuknagar, Dumuria, Khulna - 9252",
-          jobTitle: "Python Django Developer",
-          nationality: "Bangladesh",
         }),
       }}
     />
