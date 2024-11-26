177901150e31b19108d6c13150c11fe306c17884
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-11-26 10:08:59 +0600

    skill update

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,232 +1,59 @@
-import Image from "next/image";
-import React from "react";
-import HTML from "../public/skills/html.png";
-import CSS from "../public/skills/css.png";
-import BootStrap from "../public/skills/bootstrap.png";
-import TailWind from "../public/skills/tailwindcss.png";
-import Python from "../public/skills/python.png";
-import Django from "../public/skills/django.png";
-import Redis from "../public/skills/redis.png";
-import Celery from "../public/skills/celery.png";
-import Drf from "../public/skills/drf.png";
-import pagespeed from "../public/skills/pagespeed.png";
-import ubuntu from "../public/skills/ubuntu.webp";
-import next from "../public/skills/nextjs.webp";
-import Docker from "../public/skills/docker.png";
-import TypeScript from "../public/skills/typescript.png";
-import CCNA from "../public/skills/ccna.png";
-import MTCNA from "../public/skills/mtcna.png";
-import COMPTIA from "../public/skills/comptia.svg";
-import SECURITY from "../public/skills/security.png";
+// Skills.js
+import React, { useState } from "react";
+import { skillCategories } from "../data/skillData";
+import SkillCard from "./SkillCard";
 
 const Skills = () => {
+  const [selectedCategory, setSelectedCategory] = useState("web");
+
   return (
-    <div id="skills" className="w-full lg:h-screen p-2 ">
+    <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
         <div className="text-xl font-bold tracking-widest uppercase text-indigo-500">
           <p>Skills</p>
         </div>
 
         <h2 className="py-4">What I learnt</h2>
+
+        {/* Buttons for selecting categories */}
+        <div className="flex space-x-4 py-4">
+          <button
+            onClick={() => setSelectedCategory("web")}
+            className={`px-4 py-2 rounded ${
+              selectedCategory === "web"
+                ? "bg-indigo-500 text-white"
+                : "bg-gray-200"
+            }`}
+          >
+            Web Development
+          </button>
+          <button
+            onClick={() => setSelectedCategory("networking")}
+            className={`px-4 py-2 rounded ${
+              selectedCategory === "networking"
+                ? "bg-indigo-500 text-white"
+                : "bg-gray-200"
+            }`}
+          >
+            Networking
+          </button>
+          <button
+            onClick={() => setSelectedCategory("itSupport")}
+            className={`px-4 py-2 rounded ${
+              selectedCategory === "itSupport"
+                ? "bg-indigo-500 text-white"
+                : "bg-gray-200"
+            }`}
+          >
+            IT Support
+          </button>
+        </div>
+
+        {/* Skills Grid */}
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={CCNA} alt="CCNA" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>CCNA</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={MTCNA} alt="MTCNA" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>MTCNA</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={COMPTIA} alt="COMPTIA" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>COMPTIA A+</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={SECURITY} alt="SECURITY" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>SECURITY</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Python} alt="Python" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Python</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Django} alt="Django" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Django</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Drf} alt="Drf" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Django Rest Framework</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Redis} alt="Redis" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Redis</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Celery} alt="Celery" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Celery</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={next} alt="next js" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Next JS</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={HTML} alt="CSS" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={CSS} alt="CSS" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>CSS</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={TailWind} alt="TailWind" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Tailwind CSS</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={BootStrap} alt="BootStrap" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Bootstrap</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={Docker} alt="Docker" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Docker</h3>
-              </div>
-            </div>
-          </div>
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={TypeScript}
-                  alt="TypeScript"
-                  width={64}
-                  height={64}
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>TypeScript</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={pagespeed} alt="pagespeed" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Page Speed</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-6 shadow-xl shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image src={ubuntu} alt="ubuntu" width={64} height={64} />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Ubuntu</h3>
-              </div>
-            </div>
-          </div>
+          {skillCategories[selectedCategory].map((skill) => (
+            <SkillCard key={skill.name} skill={skill} />
+          ))}
         </div>
       </div>
     </div>
