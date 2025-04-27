ab9cc89a65391d034c795e63743612a8b4806a5d
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-04-27 23:18:25 +0600

    sitemap updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   siteUrl: "https://mushfiq.xyz",
   generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false, // Important to get only one sitemap.xml
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000, // No need to split
+  trailingSlash: false, // optional, if you prefer URLs without "/" at the end
   // ...other options
 };
