07a71d7eefe187e6587ad6a1dcd5bac6b080568b
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-05-02 09:16:54 +0600

    en-US

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="content-language" content="en" />
+        <meta httpEquiv="content-language" content="en-US" />
         <meta name="robots" content="follow, index" />
         <meta
           name="google-site-verification"
