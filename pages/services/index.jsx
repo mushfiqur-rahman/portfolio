8e8517478b69ed99a594b943cd57442e7cdf4d09
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-06-27 16:50:12 +0600

    service page keyword update

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -9,7 +9,10 @@ const index = () => {
         <title>Remote IT Services</title>
         <link rel="canonical" href="/services" />
         <meta name="description" content="" />
-        <meta name="keywords" content="" />
+        <meta
+          name="keywords"
+          content="dns management, dns configuration, email deliverability, email migration, data migration, freelancer, website development, domain migration, ssl certification, godaddy office 365 to microsoft 365, IMAP, squarespace, google worksapce, gsuit, office 365, remote it support, shopify developer, it support engineer"
+        />
       </Head>
       <div className="md:min-h-screen py-20">
         <ServiceCards />
