3d3968ba4f0670f0d9cd92bdc1026e00b242da3c
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-06-28 08:36:00 +0600

    hero  update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ import { FaArrowRight } from "react-icons/fa";
 export default function Hero() {
   return (
     <>
-      <div className="bg-gradient-to-br from-blue-700 via-gray-800 to-purple-800 text-white w-full h-screen text-center">
+      <div className="bg-gradient-to-br from-blue-700 via-purple-800 to-purple-800 text-white w-full h-screen text-center">
         <div className="relative max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-cente">
           <div className="mt-[10px] w-full justify-center content-center">
             <div className="">
@@ -15,7 +15,7 @@ export default function Hero() {
                 </h1>
               </div>
 
-              <div className="py-2 font-bold md:text-2xl text-sm">
+              <div className="py-2 font-bold md:text-2xl text-sm mb-4">
                 <p>
                   I help US businesses launch SEO-friendly websites and set up
                   Google Workspace or Microsoft 365 for secure, professional
@@ -23,14 +23,16 @@ export default function Hero() {
                 </p>
               </div>
 
-              <a
-                href="https://calendly.com/mushfiq.kdu/30min"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-flex items-center gap-2 bg-white text-blue-700 px-6 py-3 text-base sm:text-lg font-semibold rounded-full shadow hover:bg-gray-100 transition"
-              >
-                Book a Free Consultation <FaArrowRight />
-              </a>
+              <div className="py">
+                <a
+                  href="https://calendly.com/mushfiq.kdu/30min"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex items-center gap-2 bg-white text-blue-700 px-6 py-3 text-base sm:text-lg font-semibold rounded-full shadow hover:bg-gray-100 transition"
+                >
+                  Book a Free Consultation <FaArrowRight />
+                </a>
+              </div>
             </div>
           </div>
         </div>
