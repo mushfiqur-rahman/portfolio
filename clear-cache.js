15ce9d8d5e20dcd73dc504aa6c3735dafb657e6e
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-07-11 08:48:41 +0600

    clear cache

---
+++ clear-cache.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const path = require("path");
+
+const dir = path.join(__dirname, ".next");
+
+fs.rmSync(dir, { recursive: true, force: true });
+
+console.log(".next folder deleted");
