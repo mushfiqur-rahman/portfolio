24003c18ad48a733ae126bca8d770594359529cd
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-07-15 15:31:35 +0600

    skills border add

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -10,68 +10,68 @@ const Skills = () => {
 
         <h2 className="py-4">What I learnt</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 text-center">
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Microsoft 365 / Office 365</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Google Worksapce</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>DNS Management</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Email Migration</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Ubuntu</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Kali Linux</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>GitHub CI/CD</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>AWS</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>HTML</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>CSS</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>SCSS</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Tailwind CSS</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>BootStrap</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>React / Next JS</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Python</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Django</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Django Rest Framework</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>MySQL</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>API Development</h3>
           </div>
 
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Docker Container</h3>
           </div>
-          <div className="p-6 shadow-xl shadow-amber-200 dark:shadow-gray-200 rounded-xl hover:scale-105 ease-in duration-300">
+          <div className="p-6 shadow-xl border border-amber-400 rounded-xl hover:scale-105 ease-in duration-300">
             <h3>Pagespeed</h3>
           </div>
         </div>
