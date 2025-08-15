ebbd2f99c9d7e9ae8980be422a0032a457ad42f4
mushfiqur-rahman <mushfiq.style@gmail.com>
2025-08-15 12:13:19 +0600

    seo improved

--- pages/eventtech/index.jsx
+++ pages/eventtech/index.jsx
@@ -3,72 +3,87 @@ import React from "react";
 import eventtechImg from "../../public/projects/eventtech.webp";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
+import Head from "next/head";
 
-const eventtech = () => {
+const index = () => {
   return (
-    <div className="w-full">
-      <div className="w-screen h-[50vh] relative">
-        <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/50 z-10" />
-        <Image
-          className="absolute z-1"
-          layout="fill"
-          objectFit="cover"
-          src={eventtechImg}
-          alt="eventtechImg"
+    <>
+      <Head>
+        <title>Eventtechbd - project</title>
+        <link rel="canonical" href="/eventtech" />
+        <meta
+          name="description"
+          content="This was first project which is developed for a startup company based in jessore khulna bangladesh"
         />
-        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Eventtechbd</h2>
-          <h3>Nextt JS / Tailwind</h3>
-        </div>
-      </div>
-
-      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
-        <div className="col-span-4">
-          <p>Project</p>
-          <h2>Overview</h2>
-          <p>
-            I built this application in Next JS and is hosted on Netlify. This
-            app made for a strtup software company in Bangladesh. It features
-            dark & light theme, youtube video embed, scroll to top and slider
-            using trending swiper js
-          </p>
-          <a
-            href="https://github.com/mushfiqur-rahman/eventtech"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className="px-8 py-2 mt-4 mr-8">Source Code</button>
-          </a>
-          <a
-            href="https://eventtechbd.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className="px-8 py-2 mt-4">Live</button>
-          </a>
-        </div>
-        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
-          <div className="p-2">
-            <p className="text-center font-bold pb-2">Technologies</p>
-            <div className="grid grid-cols-3 md:grid-cols-1">
-              <p className="text-gray-600 py-2 flex items-center dark:text-slate-200">
-                <RiRadioButtonFill className="pr-1" /> NextJS
-              </p>
-              <p className="text-gray-600 py-2 flex items-center dark:text-slate-200">
-                <RiRadioButtonFill className="pr-1" /> Tailwind
-              </p>
-              <p className="text-gray-600 py-2 flex items-center dark:text-slate-200">
-                <RiRadioButtonFill className="pr-1" /> Javascript
-              </p>
-            </div>
+        <meta
+          name="keywords"
+          content="Next js, React js, Framer Motion, frontend"
+        />
+      </Head>
+      <div className="w-full">
+        <div className="w-screen h-[50vh] relative">
+          <div className="absolute top-0 left-0 w-full h-[50vh] bg-black/50 z-10" />
+          <Image
+            className="absolute z-1"
+            layout="fill"
+            objectFit="cover"
+            src={eventtechImg}
+            alt="eventtechImg"
+          />
+          <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+            <h2 className="py-2">Eventtechbd</h2>
+            <h3>Nextt JS / Tailwind</h3>
           </div>
         </div>
-        <Link href="/#project">
-          <p className="underline cursor-pointer">Back</p>
-        </Link>
+
+        <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+          <div className="col-span-4">
+            <p>Project</p>
+            <h2>Overview</h2>
+            <p>
+              I built this application in Next JS and is hosted on Netlify. This
+              app made for a strtup software company in Bangladesh. It features
+              dark & light theme, youtube video embed, scroll to top and slider
+              using trending swiper js
+            </p>
+            <a
+              href="https://github.com/mushfiqur-rahman/eventtech"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="px-8 py-2 mt-4 mr-8">Source Code</button>
+            </a>
+            <a
+              href="https://eventtechbd.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className="px-8 py-2 mt-4">Live</button>
+            </a>
+          </div>
+          <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4">
+            <div className="p-2">
+              <p className="text-center font-bold pb-2">Technologies</p>
+              <div className="grid grid-cols-3 md:grid-cols-1">
+                <p className="text-gray-600 py-2 flex items-center dark:text-slate-200">
+                  <RiRadioButtonFill className="pr-1" /> NextJS
+                </p>
+                <p className="text-gray-600 py-2 flex items-center dark:text-slate-200">
+                  <RiRadioButtonFill className="pr-1" /> Tailwind
+                </p>
+                <p className="text-gray-600 py-2 flex items-center dark:text-slate-200">
+                  <RiRadioButtonFill className="pr-1" /> Javascript
+                </p>
+              </div>
+            </div>
+          </div>
+          <Link href="/#project">
+            <p className="underline cursor-pointer">Back</p>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default eventtech;
+export default index;
